38574d31343927491bf8350aa4cd62d553526b63
huuuunny <doucet.yohann@gmail.com>
2023-09-22 17:22:37 +0200

    ajout contraintes sur modele

--- src/main/java/com/yohann/api/model/Employee.java
+++ src/main/java/com/yohann/api/model/Employee.java
@@ -6,8 +6,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 
+
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -17,17 +19,21 @@ public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull
     private Long id;
 
+    @NotEmpty(message = "First name may not be empty")
+    @Size(min = 2, max = 32, message = "First name must be between 2 and 32 characters long")
     @Column(name="first_name")
     private String firstName;
 
     @Column(name="last_name")
     private String lastName;
 
+    @NotEmpty(message = "Mail may not be empty")
     private String mail;
 
+    @NotEmpty(message = "Password may not be empty")
+    @Size(min = 6, message = "Password must be minimum 6 characters long")
     private String password;
 
 }
